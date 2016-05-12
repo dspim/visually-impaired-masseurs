637a570d6a31b239bb8d52afc6d2a6e241b3935c
NoelBao <noeleon930@gmail.com>
2016-05-12 21:23:21 +0800

    plz

--- webapp/js/idmt.js
+++
@@ -1,8 +0,0 @@
-function toIdmt() {
-	// Style
-	menu_idmt.addClass('active');
-	menu_summary.removeClass('active');
-	menu_log.removeClass('active');
-
-	$('.mode_idmt').toggle(0);
-}
--- webapp/js/summary.js
+++
@@ -1,8 +0,0 @@
-function toSummary() {
-	// Style
-	menu_summary.addClass('active');
-	menu_idmt.removeClass('active');
-	menu_log.removeClass('active');
-
-	$('.mode_idmt').toggle(0);
-}
