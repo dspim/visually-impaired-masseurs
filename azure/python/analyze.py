4a91d04310f4b49e84daea0ba0d5f443bb5dd294
blackzid <blackzidd@gmail.com>
2016-06-01 19:57:48 +0800

    update python

--- azure/python/analyze.py
+++ azure/python/analyze.py
@@ -10,6 +10,7 @@ import pymysql
 import itertools
 import plotly
 import calendar
+import json
 # plotly.offline.init_notebook_mode()
 from plotly.graph_objs import *
 from plotly.offline.offline import _plot_html
@@ -20,7 +21,10 @@ EnglishToChinese = {"assigned":"指定節數", "not_assigned": "未指定節數"
 
 def apply_lambda(compares):
     if len(compares) == 1:
-        return (lambda x, y: x + y[table[compares[0]]])
+        if compares[0] == "assigned_not_assigned":
+            return (lambda x, y: x + y[table["assigned"]] + y[table["not_assigned"]])
+        else:
+            return (lambda x, y: x + y[table[compares[0]]])
     elif "assigned" in compares and "not_assigned" in compares:
         return (lambda x, y: x + y[table["assigned"]] + y[table["not_assigned"]])
 def add_months(sourcedate,months):
@@ -30,11 +34,11 @@ def add_months(sourcedate,months):
     day = min(sourcedate.day,calendar.monthrange(year,month)[1])
     return date(year,month,day)
 
-def query(compares, targets, between, by, chartMode, barMode, pieMode, sortBy):
+def query(compares, targets, between, by, chartMode, barMode, pieMode, sortBy, jsonfile):
     # compares = ['assigned', 'not_assigned', 'guest']
     # target = {"m":"","h":"","s": "","p":""}
     # between = 'masseur'|'helper'|'shop'|'date'
-    conn = pymysql.connect(host='dream.cs.nccu.edu.tw', port=32781, user='root', passwd='d4sg', db='d4sg',charset='utf8')
+    conn = pymysql.connect(host='dream.cs.nccu.edu.tw', port=32769, user='root', passwd='d4sg', db='d4sg',charset='utf8')
     cur = conn.cursor()
     cur.execute("SELECT wid,mid,hid,sid,assigned,not_assigned,guest_num,log_date FROM worklog")  
     worklogs = list(cur.fetchall())
@@ -193,12 +197,12 @@ def query(compares, targets, between, by, chartMode, barMode, pieMode, sortBy):
             values = {}
             for compare in compares:
                 if by == "sum":
-                    values[compare] = reduce(lambda a, b: a + b[table[compare]],logs , 0)
+                    values[compare] = reduce(apply_lambda([compare]),logs , 0)
                 elif by == "count":
                     values[compare] = len(logs)
                 elif by == "average":
                     if len(logs) != 0:
-                        values[compare] = reduce(lambda a, b: a + b[table[compare]],logs , 0)/float(len(logs))
+                        values[compare] = reduce(apply_lambda([compare]),logs , 0)/float(len(logs))
             tmp.append((name, values))
         # Sort List by SORTBY
         if sortBy != None:
@@ -206,58 +210,74 @@ def query(compares, targets, between, by, chartMode, barMode, pieMode, sortBy):
                 tmp = sorted(tmp, key=lambda a: a[1][sortBy], reverse=True)
             elif sortBy == "assigned_not_assigned":
                 tmp = sorted(tmp, key= lambda a: a[1]["assigned"]+a[1]["not_assigned"], reverse=True)
-        
-        x = map(lambda x: x[0], tmp)
-        y = {}
-        for compare in compares:
-            y[compare] = map(lambda a: a[1][compare], tmp)
-            
-        if chartMode == "pie":
-            if pieMode == "sum" and"assigned" in compares and "not_assigned" in compares:
-                length = len(y["assigned"])
-                values = []
-                for i in range(0, length):
-                    values.append(y["assigned"][i]+y["not_assigned"][i])
-                data.append({
-                    "labels":x,
-                    "values":values,
-                    "type":"pie",
-                    "name":EnglishToChinese["assigned_not_assigned"],
-                    "hoverinfo":"label+percent+name"
-                })
-                layout = {}
-            elif pieMode == "split":
-                iterator = 0
+        if jsonfile:
+            jsonArray = []
+            fieldname = ""
+            if between == "masseur" or between == "helper":
+                fieldname = "人名"
+            elif between == "店名":
+                fieldname = "店名"
+            else:
+                fieldname = "時間"
+            for item in tmp:
+                obj = {}
+                obj[fieldname] = item[0]
                 for compare in compares:
-                    iterator += 1 
-                    xLeft = float(1)/len(compares) * (iterator-1)
-                    xRight = float(1)/len(compares) * iterator
+                    obj[compare] = item[1][compare]
+                jsonArray.append(obj)
+            print json.dumps(jsonArray,sort_keys=True, indent=4, separators=(',', ': '))
+        else:
+            x = map(lambda x: x[0], tmp)
+            y = {}
+            for compare in compares:
+                y[compare] = map(lambda a: a[1][compare], tmp)
+
+            if chartMode == "pie":
+                if pieMode == "sum" and"assigned" in compares and "not_assigned" in compares:
+                    length = len(y["assigned"])
+                    values = []
+                    for i in range(0, length):
+                        values.append(y["assigned"][i]+y["not_assigned"][i])
                     data.append({
                         "labels":x,
-                        "values":y[compare],
+                        "values":values,
                         "type":"pie",
-                        "name":EnglishToChinese[compare],
-                        "domain": {'x': [xLeft, xRight],
-                           'y': [0,0.8]},
+                        "name":EnglishToChinese["assigned_not_assigned"],
                         "hoverinfo":"label+percent+name"
                     })
-                layout = {}
-        elif chartMode == "bar":
-            for compare in compares:
-                trace = Bar(
-                    x = x,
-                    y = y[compare],
-                    name = EnglishToChinese[compare]
+                    layout = {}
+                elif pieMode == "split":
+                    iterator = 0
+                    for compare in compares:
+                        iterator += 1 
+                        xLeft = float(1)/len(compares) * (iterator-1)
+                        xRight = float(1)/len(compares) * iterator
+                        data.append({
+                            "labels":x,
+                            "values":y[compare],
+                            "type":"pie",
+                            "name":EnglishToChinese[compare],
+                            "domain": {'x': [xLeft, xRight],
+                               'y': [0,0.8]},
+                            "hoverinfo":"label+percent+name"
+                        })
+                    layout = {}
+            elif chartMode == "bar":
+                for compare in compares:
+                    trace = Bar(
+                        x = x,
+                        y = y[compare],
+                        name = EnglishToChinese[compare]
+                    )
+                    data.append(trace)
+                layout = Layout(
+                    barmode= barMode
                 )
-                data.append(trace)
-            layout = Layout(
-                barmode= barMode
-            )
 
-        fig = Figure(data=data, layout=layout)
-        plot_html, plotdivid, width, height = _plot_html(fig, False, "", True, '100%', 525, False)
-#         plotly.offline.plot(fig)
-        print plot_html
+            fig = Figure(data=data, layout=layout)
+            plot_html, plotdivid, width, height = _plot_html(fig, False, "", True, '100%', 525, False)
+    #         plotly.offline.plot(fig)
+            print plot_html
     cur.close()
     conn.close()
     
@@ -279,6 +299,8 @@ parser.add_argument("--barMode", help="Select one bar chart mode , including sta
 parser.add_argument("--pieMode", help="Select one bar pie mode , including sum, split. ex.: --pieMode sum", default="sum")
 
 parser.add_argument("--sortBy", help="Sort bar chart by COMPARE, assigned_not_assigned. ex.: --sortBy assigned_not_assigned")
+parser.add_argument("--jsonfile", help="output json file.", action='store_true')
+
 args = parser.parse_args()
 if args.compare != None:
     compares = args.compare.split(',')
@@ -290,8 +312,7 @@ if step != "":
     between = {"from":datetime.strptime(args.fromDate,"%Y-%m-%d"), "to":datetime.strptime(args.toDate,"%Y-%m-%d"), "step":step}
 elif args.fromDate != None and args.toDate != None:
     target_period = {"from":datetime.strptime(args.fromDate,"%Y-%m-%d"), "to":datetime.strptime(args.toDate,"%Y-%m-%d")}
-
-query(compares, {"m":args.masseur,"h":args.helper,"s":args.shop,"p":target_period}, between, args.by, args.chartMode, args.barMode, args.pieMode, args.sortBy)
+query(compares, {"m":args.masseur,"h":args.helper,"s":args.shop,"p":target_period}, between, args.by, args.chartMode, args.barMode, args.pieMode, args.sortBy, args.jsonfile)
 
 
 # In[ ]:
